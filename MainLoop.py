070d38c0cdfa54dd790528fabaaf30d6d9facb43
Elia-Boscaini <57223825+Elia-Boscaini@users.noreply.github.com>
2021-03-08 18:58:57 +0100

    Update MainLoop.py

--- MainLoop.py
+++ MainLoop.py
@@ -4,7 +4,6 @@
 # 9 illegal space
 # 1-6 players
 
-
 import pygame as pg
 from Board import Board
 
@@ -13,18 +12,18 @@ clock = pg.time.Clock()
 running = True
 window = pg.display.set_mode((640*2, 480*2))
 window.fill((255, 255, 255))
-btn = pg.Rect(0, 0, 100, 30)
-rect1 = pg.Rect(0, 30, 100, 100)
-COLOR1=(0,0,255)
-COLOR2=(0,255,255)
-COLOR3=(255,255,255)
-COLOR4=(0,0,0)
-COLOR5=(255,0,0)
-COLOR6 = (255,255,0)
+rectHeight = 50
+rectWidth = 50
+
 players = 2
-
-
 b = Board(players)
+
+COLOR0=(0,0,255)
+COLOR1=(0,255,255)
+COLOR2=(255,255,255)
+COLOR9=(0,0,0)
+COLOR11=(255,0,0)
+COLOR12 = (255,255,0)
 
 def validMoveWalk(b , x, y):
     if b.getEntry(x+1,y+1) == 0:
@@ -40,76 +39,78 @@ def validMoveWalk(b , x, y):
 
 def validMoveJump(b, x, y):
     #add all players 3-6
-    if (b.getEntry(x+1,y+1) == 1) or (b.getEntry(x+1,y+1) == 2):
-        if (b.getEntry(x+2,y+2) == 0):
-            b.setEntry(x+2,y+2, 12)
-            validMoveJump(b,x+2,y+2)
-    if (b.getEntry(x-1,y-1) == 1) or (b.getEntry(x-1,y-1) == 2):
-        if (b.getEntry(x-2,y-2) == 0):
-            b.setEntry(x-2,y-2, 12)
-            validMoveJump(b,x-2,y-2)
+    if b.getEntry(x+1, y+1) in range(1, 7):
+        if (b.getEntry(x+2, y+2) == 0):
+            b.setEntry(x+2, y+2, 12)
+            validMoveJump(b, x+2, y+2)
+        
+    if b.getEntry(x-1, y-1) in range(1, 7):
+        if (b.getEntry(x-2, y-2) == 0):
+            b.setEntry(x-2, y-2, 12)
+            validMoveJump(b, x-2, y-2)
 
-    if (b.getEntry(x+1,y-1) == 1) or (b.getEntry(x+1,y-1) == 2):
-        if (b.getEntry(x+2,y-2) == 0):
-            b.setEntry(x+2,y-2, 12)
-            validMoveJump(b,x+2,y-2)
+    if b.getEntry(x+1, y-1) in range(1, 7):
+        if (b.getEntry(x+2, y-2) == 0):
+            b.setEntry(x+2, y-2, 12)
+            validMoveJump(b, x+2, y-2)
 
-
-    if (b.getEntry(x-1,y+1) == 1) or (b.getEntry(x-1,y+1) == 2):
-        if (b.getEntry(x-2,y+2) == 0):
-            b.setEntry(x-2,y+2, 12)
-            validMoveJump(b,x-2,y+2)
+    if b.getEntry(x-1, y+1) in range(1, 7):
+        if (b.getEntry(x-2, y+2) == 0):
+            b.setEntry(x-2, y+2, 12)
+            validMoveJump(b, x-2, y+2)
+            
 def movePiece(b, x , y):
-    b.setEntry(x,y,b.currentPlayer)
+    b.setEntry(x, y, b.currentPlayer)
     b.resetAfterMoves()
     b.currentPlayerIncrement()
     if b.checkWin():
         print("WIN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-rectangleHeight = 50
-rectangleWidth = 50
-
 while running:
     clock.tick(60)
     window.fill((255, 255, 255))
     for e in pg.event.get():
+        
         if e.type == pg.MOUSEBUTTONDOWN:
             (mouseX, mouseY) = pg.mouse.get_pos()
-            print(mouseY//rectangleWidth,mouseX//rectangleHeight)
-            if b.getEntry(mouseY//rectangleWidth,mouseX//rectangleHeight) == b.currentPlayer:
+            (xCoord, yCoord) = (mouseY//rectWidth, mouseX//rectHeight)
+            print(xCoord, yCoord)
+            if b.getEntry(xCoord, yCoord) == b.currentPlayer:
                 b.resetPossibleMoves()
-
-                b.setEntry(mouseY//rectangleWidth,mouseX//rectangleHeight, 11)
-                validMoveWalk(b,mouseY//rectangleWidth,mouseX//rectangleHeight)
-                validMoveJump(b,mouseY//rectangleWidth,mouseX//rectangleHeight)
-            elif b.getEntry(mouseY//rectangleWidth,mouseX//rectangleHeight) == 11:
+                b.setEntry(xCoord, yCoord, 11)
+                validMoveWalk(b, xCoord, yCoord)
+                validMoveJump(b, xCoord, yCoord)
+                
+            elif b.getEntry(xCoord, yCoord) == 11:
                 b.resetPossibleMoves()
-            elif b.getEntry(mouseY//rectangleWidth,mouseX//rectangleHeight) == 12:
-                movePiece(b,mouseY//rectangleWidth,mouseX//rectangleHeight)
-
-
+                
+            elif b.getEntry(xCoord, yCoord) == 12:
+                movePiece(b, xCoord, yCoord)
+                
         if e.type == pg.QUIT:
             running = False
-    #end event handling
+            
+    #board printing
     for i in range(b.length1):
         for j in range(b.length2):
             if b.getEntry(i, j) == 0 :
-                pg.draw.rect(window,COLOR1,(j*rectangleHeight,i*rectangleWidth,rectangleHeight,rectangleWidth))
+                pg.draw.rect(window, COLOR0, (j*rectHeight, i*rectWidth, rectHeight, rectWidth))
 
             elif b.getEntry(i, j) == 1:
-                pg.draw.rect(window,COLOR2,(j*rectangleHeight,i*rectangleWidth,rectangleHeight,rectangleWidth))
+                pg.draw.rect(window, COLOR1, (j*rectHeight, i*rectWidth, rectHeight, rectWidth))
 
             elif  b.getEntry(i, j) == 2:
-                pg.draw.rect(window,COLOR3,(j*rectangleHeight,i*rectangleWidth,rectangleHeight,rectangleWidth))
+                pg.draw.rect(window, COLOR2, (j*rectHeight, i*rectWidth, rectHeight, rectWidth))
 
             elif  b.getEntry(i, j) == 11:
-                pg.draw.rect(window,COLOR5,(j*rectangleHeight,i*rectangleWidth,rectangleHeight,rectangleWidth))
+                pg.draw.rect(window, COLOR11, (j*rectHeight, i*rectWidth, rectHeight, rectWidth))
+                
             elif  b.getEntry(i, j) == 12:
-                pg.draw.rect(window,COLOR6,(j*rectangleHeight,i*rectangleWidth,rectangleHeight,rectangleWidth))
+                pg.draw.rect(window, COLOR12, (j*rectHeight, i*rectWidth, rectHeight, rectWidth))
+                
             else:
-                pg.draw.rect(window,COLOR4,(j*rectangleHeight,i*rectangleWidth,rectangleHeight,rectangleWidth))
+                pg.draw.rect(window, COLOR9, (j*rectHeight, i*rectWidth, rectHeight, rectWidth))
 
     pg.display.flip()
 
-#end main loop
 pg.quit()
